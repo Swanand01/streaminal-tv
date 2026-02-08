86c6ace0fa2ffaff7e25891dc1b01880d9bc499e
Swanand01 <swanandmathekar@gmail.com>
2026-02-08 20:31:13 +0530

    change similar media columns

--- app/tv-shows/[slug]/similar-tv-shows-section.tsx
+++ app/tv-shows/[slug]/similar-tv-shows-section.tsx
@@ -11,7 +11,7 @@ export function SimilarTVShowsSection({ shows }: SimilarTVShowsSectionProps) {
   return (
     <aside className="w-full lg:block lg:w-80 xl:w-96">
       <h2 className="mb-4 text-xl font-bold">More Like This</h2>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
         {shows.slice(0, 6).map((item) => (
           <MediaCard key={item.id} media={item} variant="grid" showMediaType={false} />
         ))}
